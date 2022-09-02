e220085ddcd6eb13a62900beb590cfe027c5f4e0
EliGolam <elistasibusiness@gmail.com>
2022-09-02 17:01:46 +0200

    v1.0: Functional website

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+// Initial Handshake
+console.log('DEBUG - script.js: OK!');
+
+// Global variables and initializations
+const startBtn = document.getElementById('startButton');
+
+// Start button on click
+startBtn.addEventListener('click', () => {
+    console.log('DEBUG - startBtn: CLICKED');
+
+    // Local variables
+    const cellAmount = 100;
+    const mainBox = document.getElementById('main-box');
+
+    // Create cells within the box
+    for (let i = 0; i < cellAmount; i++){
+        const cell = createSquare(i);
+
+        mainBox.append(cell);
+    }
+})
+
+
+
+// FUNCTIONS
+function createSquare(value) {
+    value = (value === null || typeof value === undefined) ? '' : value
+
+    // Function Local Variables
+    const square = document.createElement('div');
+    square.classList.add('box-cell', 'd-flex', 'justify-content-center', 'align-items-center');
+
+    const content = document.createElement('p');
+    content.classList.add('text-center', 'm-0');
+
+    if(isAnInt(value)){
+        content.innerHTML = value;
+        console.log(content.innerHTML);
+    }
+    
+    square.append(content);
+    console.log('DEBUG - cell created', value); 
+
+    square.addEventListener('click', popBoxes);
+
+    return square;
+}
+
+
+function popBoxes() {
+    console.log(this);
+    console.log(this.children[0].innerHTML);
+
+    if(!this.classList.contains('popped')) {
+        this.classList.add('popped');
+        if (value % 2 == 0) {
+            this.classList.add('bg-success', 'text-dark');
+        } else {
+            this.classList.add('bg-warning', 'text-dark');
+        }
+                
+    } else {
+        this.classList.remove('popped');
+        this.classList.remove('bg-success', 'bg-warning', 'text-dark');
+    }
+
+    console.log(this.classList);
+}
+
+
+function isAnInt(value){
+    return (typeof value === undefined || value === null || isNaN(parseInt(value))) ? false : true;
+}
